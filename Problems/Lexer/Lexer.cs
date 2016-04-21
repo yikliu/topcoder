85b9c8db2d77f9fe9ef42b1b5838af59cdcdb59b
Yikun Liu <Yikun.Liu@inin.com>
2016-04-21 11:52:05 -0400

    initial commit

---
+++ Problems/Lexer/Lexer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopCoder.Problems.Lexer
+{
+    public class Lexer
+    {
+        public string[] tokenize(string[] tokens, string input)
+        {
+            Array.Sort(tokens, delegate (string left, string right) {
+                return 0 - left.Length.CompareTo(right.Length);
+            });
+
+            bool IsStartWithToken;
+            List<string> consumed = new List<string>();
+            while (input.Length > 0)
+            {
+                IsStartWithToken = false;
+                foreach (string t in tokens)
+                {
+                    if (input.StartsWith(t))
+                    {
+                        if (input.Length <= t.Length)
+                        {
+                            input = "";
+                        }
+                        else
+                        {
+                            input = input.Remove(0, t.Length);
+                        }
+
+                        consumed.Add(t);
+                        IsStartWithToken = true;
+                        break;
+                    }
+                }
+
+                if (!IsStartWithToken)
+                {
+                    input = input.Remove(0, 1);
+                }
+            }
+
+            return consumed.ToArray();
+        }
+    }
+}
---
+++ Problems/Lexer/LexerTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TopCoder.Problems.Lexer
+{
+    [TestClass]
+    public class LexerTest
+    {
+        [TestMethod]
+        public void TestTokenize()
+        {
+            string[] tokens = new[] {"AbCd", "dEfG", "GhIj"};
+            string input = "abCdEfGhIjAbCdEfGhIj";
+            Lexer lexer = new Lexer();
+            string[] consumed = lexer.tokenize(tokens, input);
+            string[] expectedConsumed = new[] {"dEfG", "AbCd", "GhIj"};
+            CollectionAssert.AreEqual(expectedConsumed, consumed);
+        }
+    }
+}
