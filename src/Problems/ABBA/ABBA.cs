8d4fe7e30e2ed015619bbe92d65e4624bc1bebce
yikliu <yikliu@me.com>
2016-04-21 20:41:50 -0400

    ABBA

---
+++ src/Problems/ABBA/ABBA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Topcoder
+{
+	public class ABBA
+	{
+		string possible = "Possible";
+		string impossible = "Impossible";
+
+		public string canObtain(string initial, string target)
+		{
+			if (initial.Length > target.Length) {
+				return impossible;
+			}
+
+			if (initial.Length == target.Length) {
+				return initial == target ? possible : impossible;
+			}
+
+			if (target.EndsWith ("A")) {
+				return canObtain (initial, ReverseOp1 (target));
+			} else {
+				return canObtain (initial, ReverseOp2 (target));
+			}
+
+		}
+
+		private string ReverseOp1(string str){
+			return str.Remove (str.Length - 1);
+		}
+
+		private string ReverseOp2(string str){
+			var temp = str.Remove (str.Length - 1);
+			char[] charArray = temp.ToCharArray();
+			Array.Reverse( charArray );
+			return new string( charArray ); 
+		}
+	}
+}
+
---
+++ src/Problems/ABBA/ABBACanObtainTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Topcoder
+{
+	[TestFixture()]
+	public class ABBACanObtainTest
+	{
+		[Test()]
+		public void CanObtainTest ()
+		{
+			var possible = "Possible";
+			var impossible = "Impossible";
+			ABBA ab = new ABBA ();
+			Assert.AreEqual(possible, ab.canObtain("B", "ABBA"));
+
+			Assert.AreEqual(possible, ab.canObtain("BBBBABABBBBBBA","BBBBABABBABBBBBBABABBBBBBBBABAABBBAA"));
+
+			Assert.AreEqual (impossible, ab.canObtain ("A", "BB"));
+		}
+	}
+}
+
